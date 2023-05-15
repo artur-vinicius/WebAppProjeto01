361c47051cbe2ee21a0f89b256928c9d2cbabd90
Artur Vinicius <artur.t@academico.ifrn.edu.br>
2023-05-15 14:28:36 -0300

    Home

--- WebAppProjeto01/Context/EFContext.cs
+++ WebAppProjeto01/Context/EFContext.cs
@@ -12,5 +12,8 @@ namespace WebAppProjeto01.Context
         public EFContext() : base("Asp_Net_MVC_CS") { }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Home> Homec { get; set; }
+
+        public System.Data.Entity.DbSet<WebAppProjeto01.Models.Home> Homes { get; set; }
     }
 }
---
+++ WebAppProjeto01/Models/Home.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto01.Models
+{
+    public class Home
+    {
+        public IEnumerable<Fabricante> fabricantes { get; set; }
+        public IEnumerable<Categoria> categorias { get; set; }
+    }
+}
